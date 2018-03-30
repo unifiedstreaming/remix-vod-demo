3d59bd945891dc7af7e737240302af0b94d0954d
Mark Ogle <mark@unified-streaming.com>
2018-03-30 11:04:22 +0200

    change pull_images.sh to pull 1.7.32

--- pull_images.sh
+++ pull_images.sh
@@ -1,3 +1,2 @@
-docker pull unifiedstreaming/origin:1.8.3
-docker pull unifiedstreaming/packager:1.8.3
-docker pull unifiedstreaming/unified-remix:1.8.3
+docker pull unifiedstreaming/origin:1.7.32
+docker pull unifiedstreaming/packager:1.7.32
